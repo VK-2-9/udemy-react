be0e4915e3800ab5757371abc92434e0155e3b99
VK-2-9 <vishalnath002@gmail.com>
2025-03-27 06:06:33 +0530

    done

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ReactDOM from "react-dom/client"
 
 import "./style.css"
 
-import App  from "./app"
+import App  from "./App"
 
 const root= ReactDOM.createRoot(document.getElementById("root"))
 
